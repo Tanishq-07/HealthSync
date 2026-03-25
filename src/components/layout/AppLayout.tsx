004654a51b95633302e7bee26f6571e9356dcc31
Tanishq-07 <tanishqsoni1563@gmail.com>
2026-03-26 00:57:27 +0530

    Fixed navbar moving issue

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,17 +4,22 @@ import Navbar from '../common/Navbar';
 import Sidebar from '../common/Sidebar';
 
 const AppLayout: React.FC = () => (
-  <div className="flex flex-col min-h-screen bg-slate-50">
-    <Navbar />
-    <div className="flex flex-1">
+  <div className="flex flex-col h-screen overflow-hidden bg-slate-50">
+    
+    {/* Sticky Navbar */}
+    <div className="shrink-0 z-30 sticky top-0">
+      <Navbar />
+    </div>
 
-      <div className="hidden md:block w-56 shrink-0">
-        <div className="fixed top-16 left-0 w-56 h-[calc(100vh-4rem)] bg-white border-r border-slate-200 overflow-hidden">
-          <Sidebar />
-        </div>
+    <div className="flex flex-1 overflow-hidden">
+
+      {/* Desktop Sidebar */}
+      <div className="hidden md:block w-56 shrink-0 bg-white border-r border-slate-200 overflow-hidden">
+        <Sidebar />
       </div>
 
-      <main className="flex-1 overflow-y-auto min-w-0 min-h-[calc(100vh-4rem)] bg-slate-50">
+      {/* Main content */}
+      <main className="flex-1 overflow-y-auto min-w-0 bg-slate-50">
         <div className="p-4 md:p-6 max-w-full">
           <Outlet />
         </div>
